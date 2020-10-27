0e763b85ef7c2ef7f3b5f3e424e9520768828e11
Exequiel Salinas <exesalinas393@gmail.com>
2020-10-27 02:10:32 -0300

    Agregar archivos de proyecto.

---
+++ Typewriter/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Typewriter
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+
+    {
+        Double velocidad = new Double();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Presentacion pres = new Presentacion();
+            pres.Show();
+            pres.SetString(textBoxPrincipal.Text);
+
+
+        }
+
+        private void SliderVelocidad_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Slider slider = sender as Slider;
+            velocidad = SliderVelocidad.Value;
+            txtVelocidad.Text =  "Velocidad : " +velocidad.ToString("0");
+            
+        }
+    }
+}
+
---
+++ Typewriter/Presentacion.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Typewriter
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Presentacion : Window
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        private string stringToDisplay = ;
+
+        private Random rand;
+
+        private int whichLetter = 0;
+
+        public Presentacion()
+
+        {
+
+            InitializeComponent();
+
+            rand = new Random(System.DateTime.Now.Millisecond);
+
+            this.timer.Interval = new TimeSpan(0, 0, 0, 0, ); //initially it will trigger at 200 milliseconds
+
+            this.timer.Tick += new EventHandler(timerTick);
+
+            
+
+        }
+
+        public void SetString(string s)
+
+        {
+
+            whichLetter = 0;
+
+            this.stringToDisplay = s;
+
+            this.timer.Start();
+
+        }
+
+        void timerTick(object sender, EventArgs e)
+
+        {
+
+            //Let's change the time that the following letter will last to appear (just to simulate not-synchronous-writing) 
+
+            this.timer.Stop();
+
+            this.timer.Interval = new TimeSpan(0, 0, 0, 0, rand.Next(0, 200)); //the following letter will appear in a period between the next 0 and 200 milliseconds.
+
+            if (whichLetter < this.stringToDisplay.Length)
+
+            {
+
+                AddFollowingLetter();
+
+                timer.Start();
+
+            }
+
+        }
+
+        void AddFollowingLetter()
+
+        {
+            
+           txtbSample.Text += this.stringToDisplay.Substring(whichLetter, 1);
+
+            whichLetter++;
+
+        }
+
+
+    }
+}
